7eaa0cbdb300e583c0ed0ee6c43679526ac05c46
yazhu0609 <7010hope@gmail.com>
2025-07-22 14:50:27 +0800

    Add comments and modify the layout.

--- src/app/product-detail/product-detail.ts
+++ src/app/product-detail/product-detail.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; //元件生命週期
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router'; //ActivatedRoute：取得當前路由的參數
 import { HttpClient} from '@angular/common/http';
 
 @Component({
@@ -10,24 +10,25 @@ import { HttpClient} from '@angular/common/http';
   templateUrl: './product-detail.html',
   styleUrls: ['./product-detail.css']
 })
-export class ProductDetailComponent implements OnInit {
-  product: any = null;
+
+export class ProductDetailComponent implements OnInit { //實作 OnInit 介面
+  product: any = null; //儲存 API 回傳的特定商品資料
   quantity = 1;
   maxStock = 1;
   userPoints = 1000;
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
-  ngOnInit(): void {
-    const productId = this.route.snapshot.paramMap.get('id');
+  ngOnInit(): void { //初始化操作，自動執行此函式
+    const productId = this.route.snapshot.paramMap.get('id');//從目前的路由快照中，抓出網址中的 id 參數
     this.http.get<any>('https://function-1-6gjnjd5fqa-as.a.run.app/product')
       .subscribe(res => {
-        this.product = res.data.find((p: any) => p.productId === productId);
-        this.maxStock = this.product?.stock || 1;
+        this.product = res.data.find((p: any) => p.productId.toString() === productId);
+        this.maxStock = this.product?.stock || 1; //如果product存在，就回傳product.stock，否則回傳1
       });
   }
 
-  changeQty(delta: number) {
+  changeQty(delta: number) { //變化量：+1 & -1
     const next = this.quantity + delta;
     if (next >= 1 && next <= this.maxStock) {
       this.quantity = next;
--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClient} from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core'; //元件生命週期
+import { CommonModule } from '@angular/common'; //引入 *ngIf、*ngFor
+import { HttpClient} from '@angular/common/http'; //引入 GET / POST
+import { RouterModule } from '@angular/router'; //引入 [routerLink]
 
 @Component({
   selector: 'app-product-list',
@@ -10,16 +10,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './product-list.html',
   styleUrls: ['./product-list.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit { //實作 OnInit
   products: any[] = [];
   loading = true;
   error = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}//相依注入
 
-  ngOnInit(): void {
+  ngOnInit(): void { //初始化時呼叫 
     this.http.get<any>('https://function-1-6gjnjd5fqa-as.a.run.app/product')
-      .subscribe({
+      .subscribe({ //監聽API回傳結果
         next: res => {
           this.products = res.data || [];
           this.loading = false;
@@ -32,3 +32,10 @@ export class ProductListComponent implements OnInit {
       });
   }
 }
+
+//any    任意型別，取消型別檢查
+//unknow 任意型別，保留型別檢查
+//void   函式沒有回傳值
+
+//GET：向指定的資源要求資料，類似於查詢操作
+//POST：將要處理的資料提交給指定的資源，類似於更新操作。
